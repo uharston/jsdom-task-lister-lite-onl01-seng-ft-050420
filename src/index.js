d7d7ea225aabf2563795916c06f0ea5bdbd230f0
uharston <uharston@gmail.com>
2020-08-10 09:37:57 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById('create-task-form'); 
+  form.addEventListener("submit", createNewTask);
+})
+
+function createNewTask(event) {
+  event.preventDefault();
+  const newTask = document.getElementById('new-task-description');
+  const uList = document.getElementById('tasks');
+  const newListItem = document.createElement('li');
+
+  newListItem.innerText = newTask.value 
+  uList.appendChild(newListItem)
+  
+  event.target.reset();
+}
+
+
+
+
+
+
+
+
+
